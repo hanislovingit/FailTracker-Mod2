772d53cf1ee41be39ccb3a17addb92ea2fe04de4
hanislovingit <hanislovingit@gmail.com>
2015-07-17 17:38:57 -0500

    FailTracker initial commit - Module 2

---
+++ FailTracker.Web/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FailTracker.Web.Data;
+
+namespace FailTracker.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+	    private readonly ApplicationDbContext _context;
+	    private readonly ISayHello _sayHello;
+
+	    public HomeController(ApplicationDbContext context, ISayHello sayHello)
+	    {
+	        _context = context;
+	        _sayHello = sayHello;
+	    }
+
+	    public ActionResult Index()
+		{
+			return View();
+		}
+	}
+
+    public interface ISayHello
+    {
+        
+    }
+
+    public class SayHello : ISayHello
+    {
+
+    }
+}
---
+++ FailTracker.Web/Infrastructure/StructureMapDependencyResolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StructureMap;
+
+namespace FailTracker.Web.Infrastructure
+{
+    public class StructureMapDependencyResolver: IDependencyResolver
+    {
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == null)
+            {
+                return null;
+            }
+
+            return serviceType.IsAbstract || serviceType.IsInterface
+                ? ObjectFactory.TryGetInstance(serviceType)
+                : ObjectFactory.GetInstance(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return ObjectFactory.GetAllInstances(serviceType).Cast<object>();
+        }
+    }
+}
